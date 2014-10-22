bfbd3edf670148a5443302ffaa030e66d487efac
Hans Meyer <hamster.of.dev@gmail.com>
2014-10-22 14:28:45 +0200

    Reintroducing some of my former settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,21 @@
 alias reload!='. ~/.zshrc'
+
+# vim
+alias v="vim -N"
+
+# debian
+alias update="sudo apt-get update && sudo apt-get upgrade"
+alias install="sudo apt-get install"
+alias remove="sudo apt-get autoremove"
+
+# git
+alias ga="git add"
+alias gs="git status"
+alias gp="git push"
+alias gc="git commit"
+
+# ls
+alias ls="ls --color=auto"
+alias ll="ls -alF"
+alias la="ls -A"
+alias  l="ls -CF"
