bf462847031c0567042650a51a482665d19473f0
Hans Meyer <hamster.of.dev@gmail.com>
2014-10-24 12:12:38 +0200

    SSH has logic for the prompt, which should go to promt.zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -71,13 +71,6 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 export EDITOR="vim"
-
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
