41268452ba3fb88e23c2de6ab6644484b35df71e
Hans Meyer <hamster.of.dev@gmail.com>
2014-11-10 21:13:39 +0100

    Restructuring zsh
    The prompt things now actually work as expected

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,7 @@
-autoload -U promptinit && promptinit
-autoload -U colors && colors
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
 precmd() {
-  export PROMPT="$(current_user)@$(current_machine)$(directory_name)
-$(prompt_symbol) "
-  export RPROMPT="$(git_quickinfo)$(last_exitcode)"
+    export RPROMPT='$(git_quickinfo)$(last_exitcode)'
+    export PROMPT='$(current_user)@$(current_machine)$(directory_name)$(prompt_symbol) '
+
 }
 
 current_user() {
@@ -19,9 +9,9 @@ current_user() {
     #react to running ssh session
   else
     if [[ $(id -u) -eq 0 ]]; then
-      echo "%{$fg[brightred]%}root%{$reset_color%}"
+      echo %{$fg[brightred]%}root%{$reset_color%}
     else
-      echo $(whoami)
+      echo %{$fg[green]%}$(whoami)%{$reset_color%}
     fi
   fi
 }
@@ -30,27 +20,41 @@ current_machine() {
   if [[ -n $SSH_CONNECTION ]]; then
     #react to running ssh session
   else
-    echo "%m"
+    echo %{$fg[green]%}%m%{$reset_color%}
   fi
 }
 
 directory_name() {
-  echo ":%3~"
+  echo %{$fg[magenta]%}:%3~%{$reset_color%}
 }
 
 prompt_symbol() {
-  echo "%{$reset_color%}%{%F{brightyellow}%}ᛄ%{$reset_color%}"
-}
-
-git_quickinfo() {
-  echo "$(git_branch)"
+  echo "\n%{$fg[green]%}ᛄ%{$reset_color%}"
 }
 
 last_exitcode() {
-  echo "%(?.. %{%F{brightred}%}%?⏎%{$reset_color%})"
+  echo "%(?..%{$fg[red]%} %?⏎%{$reset_color%})"
+}
+
+git_quickinfo() {
+    git status &> /dev/null
+    if [[ !$ -eq 0 ]]; then
+        echo $(git_branch) $(git_stashcount)
+    else
+        echo ''
+    fi
 }
 
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
+git_stashcount() {
+  local count
+  count=$(git stash list 2>/dev/null | wc -l)
+  if [[ $count -gt 0 ]]; then
+      echo $count
+  else
+      echo ''
+  fi
+}
