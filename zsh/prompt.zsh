abb2609e37b973d8e96dfbeebce363908d8646e8
Hans Meyer <hamster.of.dev@gmail.com>
2014-11-29 11:28:46 +0100

    Checking if git is available

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 precmd() {
-    export RPROMPT='$(git_quickinfo)$(last_exitcode)'
-    export PROMPT='$(current_user)@$(current_machine)$(directory_name)$(prompt_symbol) '
+    export RPROMPT="$(git_quickinfo)$(last_exitcode)"
+    export PROMPT="$(current_user)@$(current_machine)$(directory_name)$(prompt_symbol) "
 
 }
 
@@ -8,7 +8,7 @@ current_user() {
   if [[ -n $SSH_CONNECTION ]]; then
     #react to running ssh session
   else
-    if [[ $(id -u) -eq 0 ]]; then
+    if [[ $UID -eq 0 ]]; then
       echo %{$fg[brightred]%}root%{$reset_color%}
     else
       echo %{$fg[green]%}$(whoami)%{$reset_color%}
@@ -25,7 +25,10 @@ current_machine() {
 }
 
 directory_name() {
-  echo %{$fg[magenta]%}:%3~%{$reset_color%}
+    echo %{$fg[magenta]%}:%3~%{$reset_color%}
+    if [[ $(has_git) == true ]]; then
+        echo %{$fg[green]%}•%{$reset_color%}
+    fi
 }
 
 prompt_symbol() {
@@ -36,13 +39,26 @@ last_exitcode() {
   echo "%(?..%{$fg[red]%} %?⏎%{$reset_color%})"
 }
 
+has_git() {
+    if [[ -z $(git rev-parse --git-dir 2> /dev/null) ]]; then
+        return false
+    else
+        return true
+    fi
+}
+
 git_quickinfo() {
-    git status &> /dev/null;
-    if [[ !$ -eq 0 ]]; then
+    if [[ $(has_git) == true ]]; then
         echo $(git_branch) $(git_remote_status) $(git_stashcount)
     else
         echo ''
     fi
+    #git status &> /dev/null;
+    #if [[ !$ -eq 0 ]]; then
+    #    echo $(git_branch) $(git_remote_status) $(git_stashcount)
+    #else
+    #    echo ''
+    #fi
 }
 
 git_branch() {
@@ -53,7 +69,7 @@ git_stashcount() {
   local count
   count=$(git stash list 2>/dev/null | wc -l)
   if [[ $count -gt 0 ]]; then
-      echo "(⛁ $count)"
+      echo '(⛁ $count)'
   else
       echo ''
   fi
@@ -61,7 +77,7 @@ git_stashcount() {
 
 git_remote_status() {
     local rstatus
-    rstatus=$(git rev-list --left-right --boundary @{u}... 2>/dev/null)
+    rstatus=$(git rev-list --count --left-right --boundary @{u}... 2>/dev/null)
     echo $rstatus
     #echo $($rstatus | grep < | wc -l)⇅$($rstatus | grep > | wc -l)
 }
