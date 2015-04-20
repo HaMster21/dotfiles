88e055f73cc848f08a43b90738bfba38b6f57572
Hans Meyer <hamster.of.dev@gmail.com>
2015-04-20 20:50:10 +0200

    A lot of restructuring
    Some time has passed since I was editing these files. So for some of them I don't really remember
    what I did.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,14 @@
 # debian
-alias update="sudo apt-get update && sudo apt-get upgrade"
-alias install="sudo apt-get install"
+alias update="sudo apt update && sudo apt upgrade"
+alias install="sudo apt install"
 alias remove="sudo apt-get autoremove"
 
 # ls
 alias ls="ls --color=auto"
-alias ll="ls -alF"
+alias ll="ls -alFh"
 alias la="ls -A"
 alias  l="ls -CF"
+
+# hub as default
+
+alias git="hub"
